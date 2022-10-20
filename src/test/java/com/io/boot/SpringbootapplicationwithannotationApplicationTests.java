bd775d8a39fc2c28c8ded96c156c917b565ee334
parvesh <nautiyalparvesh2@gmail.com>
2022-10-20 14:15:23 -0400

    my first commit

---
+++ src/test/java/com/io/boot/SpringbootapplicationwithannotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapplicationwithannotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
